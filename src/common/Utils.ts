b0b0fa4021d2e4337237987b399340a50a645e14
Dae-Hwa Jeong <daehwa_jeong@outlook.com>
2020-12-03 21:43:23 +0900

    remove:replaced templates and sources

--- src/common/Utils.ts
+++
@@ -1,7 +0,0 @@
-export default class Utils {
-  static formatDate(_date: string): string {
-    const date = new Date(_date);
-
-    return `${date.getFullYear()}년 ${date.getMonth()}월 ${date.getDate()}일`;
-  }
-}
