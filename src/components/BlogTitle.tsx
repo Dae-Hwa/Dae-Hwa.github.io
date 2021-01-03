004baf522a503baf6daa5b60a9833f1be51c45e1
Dae-Hwa Jeong <daehwa_jeong@outlook.com>
2021-01-04 01:47:51 +0900

    Change title of blog page

--- src/components/BlogTitle.tsx
+++ src/components/BlogTitle.tsx
@@ -2,15 +2,18 @@ import * as React from "react";
 import { Header, Segment, Icon } from "semantic-ui-react";
 
 export default () => {
+  /*
+   * TODO: 제목 내용 동적으로 가져올 수 있도록
+   */
   return (
     <Segment vertical>
       <Header as="h2">
         <Icon name="newspaper" />
         <Header.Content>
-          Blog
+          정대화 블로그
             <Header.Subheader>
-            All about this starter kit
-            </Header.Subheader>
+            {/* All about this starter kit */}
+          </Header.Subheader>
         </Header.Content>
       </Header>
     </Segment>
